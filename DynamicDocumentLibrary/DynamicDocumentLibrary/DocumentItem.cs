cbf7d75b25c60708606facad00218c0b04b71bbd
Kenneth Munk <contact.me@kenmunk.com>
2024-03-31 00:31:38 -0700

    Designed the base document item class

--- DynamicDocumentLibrary/DynamicDocumentLibrary/DocumentItem.cs
+++ DynamicDocumentLibrary/DynamicDocumentLibrary/DocumentItem.cs
@@ -8,9 +8,48 @@ namespace DynamicDocumentLibrary
     {
         public class DocumentItem
         {
-            public string value;
+            private string key { get; set; }
+            public string name { get; set; }
+            public string value { get; set; }
 
-            public string toString() => value;
+            public DocumentItem()
+            {
+
+            }
+
+            public DocumentItem(string key, string name, string value)
+            {
+                this.key = key;
+                this.name = name;
+                this.value = value;
+            }
+
+            public string toString() => string.Format("{0},{1},{2}",key, name, value);
+
+            public bool setkey(string key)
+            {
+                if(this.key == null || this.key == string.Empty)
+                {
+                    this.key = key;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public string getkey()
+            {
+                if(this.key == null)
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return this.key;
+                }
+            }
 
         }
 
