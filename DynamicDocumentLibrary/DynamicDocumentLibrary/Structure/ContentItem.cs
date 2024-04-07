ed23a44a043ed120f707a1345b1da45af8785852
Kenneth Munk <da.munkk@gmail.com>
2024-04-06 19:22:29 -0700

    Exploring the use of a container class
    The pros are that section item classes take on a polymorphic behavior
    
    the cons are that compute and storage goes through the roof

---
+++ DynamicDocumentLibrary/DynamicDocumentLibrary/Structure/ContentItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+
+namespace DynamicDocumentLibrary.Structure
+{
+    public class ContentItem
+    {
+        private Type ContentType { get; set; }
+
+        private string ContentValue { get; set; }
+
+
+        public ContentItem()
+        {
+        }
+
+        public void StoreValue(DocumentItem docItem, Type type)
+        {
+            this.ContentType = type;
+            this.ContentValue = docItem.ToString();
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public DocumentItem GetValue()
+        {
+            if(this.ContentType == typeof(SectionItem))
+            {
+                SectionItem output = new SectionItem();
+                output.Deserialize(this.ContentValue);
+                return output;
+            }
+            else
+            {
+                DocumentItem output = new DocumentItem();
+                output.Deserialize(this.ContentValue);
+                return output;
+            }
+        }
+    }
+}
--- DynamicDocumentLibrary/DynamicDocumentLibrary/Structure/SectionItem.cs
+++ DynamicDocumentLibrary/DynamicDocumentLibrary/Structure/SectionItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
