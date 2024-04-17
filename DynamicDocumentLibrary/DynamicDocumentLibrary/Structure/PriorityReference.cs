f340cdebe2f932413979528ccb3fea2b5fb8c057
Kenneth Munk <da.munkk@gmail.com>
2024-04-16 18:20:02 -0700

    breaking out priority reference class and staging test for priority reference

---
+++ DynamicDocumentLibrary/DynamicDocumentLibrary/Structure/PriorityReference.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DynamicDocumentLibrary.Structure
+{
+    public class PriorityReference
+    {
+        [JsonInclude]
+        private string Key {  get; set; }
+
+        [JsonInclude]
+        private int Priority {  get; set; }
+
+        public PriorityReference(string key)
+        {
+            this.Key = key;
+            this.Priority = 0;
+        }
+
+        public PriorityReference(string key, int priority)
+        {
+            this.Key = key;
+            this.Priority = priority;
+        }
+
+        public PriorityReference(KeyedItem source)
+        {
+            this.assignKeyFrom(source);
+            this.Priority = 0;
+        }
+
+        public PriorityReference(KeyedItem source, int priority)
+        {
+            this.assignKeyFrom(source);
+            this.Priority = priority;
+        }
+
+        private void assignKeyFrom(KeyedItem source)
+        {
+            this.Key = source.GetKey();
+        }
+
+        public void updatePriority(int priority)
+        {
+            this.Priority = priority;
+        }
+
+        public int getPriority()
+        {
+            return this.Priority;
+        }
+
+        public string getKey()
+        {
+            return this.Key;
+        }
+    }
+}
---
+++ DynamicDocumentLibrary/DynamicDocumentLibraryUnitTests/StructureTests/PriorityReferenceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDocumentLibraryUnitTests.StructureTests
+{
+    [TestClass]
+    internal class PriorityReferenceTests
+    {
+        [TestMethod]
+        public void Test00_InitWithKey()
+        {
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test01_InitWithStringKeyValue()
+        {
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test02_FullInit()
+        {
+
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test03_InitWithSourceKey()
+        {
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test04_FullInitWithSource()
+        {
+
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test05_UpdatePriority()
+        {
+
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test06_GetPriority()
+        {
+
+            Assert.Fail("Test not made");
+        }
+
+        [TestMethod]
+        public void Test07_GetKey()
+        {
+
+            Assert.Fail("Test not made");
+        }
+    }
+}
